a0d3ff021bc7f849d316337283729b6a512a79e8
Sara <saranatourcontact@gmail.com>
2023-06-05 13:45:05 +0300

    convert to arrow function

---
+++ Introduction/Day 82/0- Convert To Arrow/toarrow.js
@@ -0,0 +1,37 @@
+/**
+ * Converted to elements instead of calling them within the functions.
+ *
+ */
+const blueBtn = document.getElementById("button");
+const alertBtn = document.getElementById("alert");
+const hoverBtn = document.getElementById("hover-this");
+const popOutVal = document.getElementById("popup-input");
+
+/**
+ * First , changed into addEventListner, as it is more readable.
+ *  onclick ==> click
+ */
+
+blueBtn.addEventListener("click", () => {
+  setBackgroundColorById("paragraph", "blue");
+});
+
+alertBtn.addEventListener("click", () => {
+  alert(popOutVal.value);
+});
+
+hoverBtn.addEventListener("mouseover", () => {
+  setBackgroundColorById("body", "red");
+});
+
+hoverBtn.addEventListener("mouseout", () => {
+  setBackgroundColorById("body", "white");
+});
+
+let getValueFromId = (id) => document.getElementById(id).value;
+
+let setBackgroundColorById = (id, color) => {
+  document.getElementById(id).style = "background-color: " + color;
+};
+
+let mouseOverFunction = (el) => (el.style = "background-color: black");
