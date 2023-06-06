e82d15388c0ca27a6bf4d3b64e6d040af109d59c
Sara <saranatourcontact@gmail.com>
2023-06-06 14:19:24 +0300

    updates

--- Introduction-js/Day 82/5- It's Time to Duel/cards.js
+++ Introduction-js/Day 82/5- It's Time to Duel/cards.js
@@ -1,7 +1,3 @@
-/**
- * No context is available for such a problem, but let's walk through it
- *
- */
 
 class Card {
   constructor(name, cost){
@@ -20,8 +16,7 @@ class UnitCards extends Card {
 
   // attack ?
   attack(target){
-    target.power -=10; 
-    target.resilience -=3;
+    target.resilience -=target.power;
     this.power += 5;
 
   }
@@ -35,7 +30,7 @@ class EffectCards extends Card {
   }
 
   // playing the game on the 'Unit Cards'; 
-  applyEffect(target) {
+  play(target) {
     if (this.stat === "resilience") {
       target.resilience += this.magnitude;
     } else if (this.stat === "power") {
@@ -52,7 +47,7 @@ console.log("Round 1 , redbelt before fight ==>", redBelt);
 
 //Make an instance of "Hard Algorithm" and play it on "Red Belt Ninja"
 const hardAlgo = new EffectCards("Hard Algorithim", 2, +3, "resilience");
-hardAlgo.applyEffect(redBelt);
+hardAlgo.play(redBelt);
 console.log("Round 1 , redbelt after fight ==>" ,redBelt);
 
 // Make an instance "Black Belt Ninja"
@@ -62,13 +57,13 @@ console.log("Round 2, black belt before fight ", blackBelt)
 
 // Make an instance of "Unhandled Promise Rejection" and play it on "Red Belt Ninja"
 const unhandledPromiseRejection = new EffectCards("Unhandled Promise Rejection",1,-2,"resilience");
-unhandledPromiseRejection.applyEffect(redBelt);
+unhandledPromiseRejection.play(redBelt);
 console.log("Round 2 , Redbelt after fight ", redBelt);
 
 
 // Make an instance of "Pair Programming" and play it on "Red Belt Ninja"
 const pairProgramming = new EffectCards("Pair Programming", 3, +2, "power");
-pairProgramming.applyEffect(redBelt);
+pairProgramming.play(redBelt);
 console.log("Round 3 , redbelt on round 3 after fight  ",redBelt);
 
 // "Red Belt Ninja" uses the attack method on "Black Belt Ninja"
