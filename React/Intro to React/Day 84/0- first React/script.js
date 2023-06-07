28eb98ca543d6f0fb2f09825cbf2c104d3b4fb42
Sara <saranatourcontact@gmail.com>
2023-06-07 13:29:01 +0300

    first react

---
+++ React/Intro to React/Day 84/0- first React/script.js
@@ -0,0 +1,4 @@
+const domContainer = document.querySelector('#root');
+const root = ReactDOM.createRoot(domContainer);
+root.render(React.createElement("h1", {}, "Our First React page has rendered"));    
+
